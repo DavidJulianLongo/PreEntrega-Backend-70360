dbefa3354891197ca327a6d3aa6834e90ce03791
davidlongo <longodavidjulian@gmail.com>
2025-03-15 19:55:10 -0300

    correcion de la ruta

--- src/modules/pets/pet.router.js
+++ src/modules/pets/pet.router.js
@@ -3,6 +3,6 @@ import { petController } from './pet.controller.js';
 
 const router = Router();
 
-router.get('/mocks/:amount', petController.createPetMock);
+router.post('/mocks/:amount', petController.createPetMock);
 
 export default router;
