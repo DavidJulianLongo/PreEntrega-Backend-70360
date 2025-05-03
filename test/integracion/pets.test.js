d7460a1cbf23d44941297b48fb817e3592dd0132
davidlongo <longodavidjulian@gmail.com>
2025-05-03 00:02:01 -0300

    terminados pet.test

--- test/integracion/pets.test.js
+++ test/integracion/pets.test.js
@@ -1,6 +1,7 @@
 import { expect } from "chai";
 import supertest from "supertest";
 
+
 const request = supertest("http://localhost:3000");
 
 describe("===== INTEGRATION TEST FOR PETS ROUTES =====", () => {
@@ -8,8 +9,9 @@ describe("===== INTEGRATION TEST FOR PETS ROUTES =====", () => {
     let adminCookie;
     let createdPetId; // 
 
+    // Primero login para obtener la cookie de admin, ya que el endpoint de pets es privado
     before(async () => {
-        // Login para obtener la auth de "admin" ya que las rutas de pets requieren ser "admin"
+        
         const loginAdmin = {
             email: "bert.emard@gmail.com",
             password: "!User123456",
@@ -23,6 +25,8 @@ describe("===== INTEGRATION TEST FOR PETS ROUTES =====", () => {
         adminCookie = response.headers["set-cookie"]?.[0]; // Guardamos la cookie
     });
 
+
+
     it("[POST] /api/pets/register - Should register a new pet", async () => {
 
         const newPet = {
@@ -34,9 +38,7 @@ describe("===== INTEGRATION TEST FOR PETS ROUTES =====", () => {
         }
 
         const response = await request.post("/api/pets/register").set("Cookie", adminCookie).send(newPet);
-        // console.log(response.statusCode);
-        // console.log(response.body);
-        // // console.log(response.error);
+        
         createdPetId = response.body.payload._id; // Guardar el ID de la mascota creada para eliminarla después
 
         // Aserciones para verificar que el registro fue exitoso
@@ -47,47 +49,77 @@ describe("===== INTEGRATION TEST FOR PETS ROUTES =====", () => {
         expect(response.body.payload.birthDate.slice(0, 10)).to.be.equal("2025-05-02");
         expect(response.body.payload.sex).to.be.equal("male");
         expect(response.body.payload.sterilized).to.be.equal(true);
-
-
-        after(async () => {
-           const res = await request.delete(`/api/pets/${createdPetId}`).set("Cookie", adminCookie);
-            console.log(res.body);
-        });
-
+        
     })
 
 
 
-    // it("[POST] /api/auth/login - Should login a user", async () => {
+    it("[PUT] /api/pets/:id - Should update a pet", async () => {
 
-    //     const userData = {
-    //         email: "alefector@gmail.com",
-    //         password: "!User123456",
-    //     }
+        const updatedPet = {
+            name: "Peter",
+            type: "cat",
+            birthDate: "2025-05-02",
+            sex: "male",
+            sterilized: false
+        }
 
-    //     const response = await request.post("/api/auth/login").send(userData);
+        const response = await request.put(`/api/pets/${createdPetId}`).set("Cookie", adminCookie).send(updatedPet);
 
-    //     // Aserciones para verificar que el login fue exitoso
-    //     expect(response.statusCode).to.be.equal(200);
-    //     expect(response.body.message).to.be.equal("Login successful");
-    //     expect(response.body.token).to.exist; 
+        // Aserciones para verificar que el registro fue exitoso
+        expect(response.statusCode).to.be.equal(200);
+        expect(response.body.payload.name).to.be.equal("Peter");
+        expect(response.body.payload.type).to.be.equal("cat");
+        //slice(0, 10) porque la fecha viene con hora y minutos y asi toma los primeros 10 caracteres
+        expect(response.body.payload.birthDate.slice(0, 10)).to.be.equal("2025-05-02");
+        expect(response.body.payload.sex).to.be.equal("male");
+        expect(response.body.payload.sterilized).to.be.equal(false);
+        
+    })
 
-    //     // Guarda la cookie de autenticación (usada para endpoints protegidos
-    //     authCookie = response.headers["set-cookie"]?.[0];
-    // });
+
+    it("[GET] /api/pets - Should get return all pets", async () => {
+        
+        const { statusCode, body } = await request.get("/api/pets");
+
+        // Aserciones para verificar que la respuesta es correcta
+        expect(statusCode).to.be.equal(200);
+        expect(body).to.have.property("payload").that.is.an("array");
+        expect(body.payload.length).to.be.greaterThan(0); // Verifica que haya al menos una mascota en la respuesta
+    
+    });
 
 
-    // //se ejecuta después de todos los tests: elimina al usuario creado
-    // after(async () => {
-    //     try {
-    //         const res = await request
-    //         .delete(`/api/users/${userTest._id}`)
-    //         .set("Cookie", authCookie); // Usar la cookie de autenticación para eliminar el usuario
-    //         console.log(res.body);
-    //     } catch (error) {
-    //         console.log(error);
-    //     }
 
-    // });
+    it("[GET] /api/pets/:id - Should get return a pet details", async () => {
+        
+        const { statusCode, body } = await request.get(`/api/pets/${createdPetId}`);
+        
+        // Aserciones para verificar que la respuesta es correcta
+        expect(statusCode).to.be.equal(200);
+        expect(body).to.have.property("payload").that.is.an("object");
+        expect(body.payload).to.have.property("name").that.is.a("string");
+        expect(body.payload).to.have.property("_id").that.equals(createdPetId);
+        expect(body.payload).to.have.property("type").that.is.a("string");
+        expect(body.payload).to.have.property("birthDate").that.is.a("string");
+        expect(body.payload).to.have.property("sex").that.is.a("string");
+        expect(body.payload).to.have.property("sterilized").that.is.a("boolean");
+    });
+
+
+    
+    it("[DELETE] /api/pets/:id - Should delete a pet", async () => {
+        
+        const { statusCode, body } = await request.delete(`/api/pets/${createdPetId}`).set("Cookie", adminCookie);
+        
+        // Aserciones para verificar que la respuesta es correcta
+        expect(statusCode).to.be.equal(200);
+        expect(body).to.have.property("payload").that.is.an("object");
+        expect(body).to.have.property("message").that.is.a("string");
+
+        // Verificamos que la mascota ya no esté en la base de datos haciendo un GET
+        const getResponse = await request.get(`/api/pets/${createdPetId}`);
+        expect(getResponse.statusCode).to.be.equal(404);
+    });
 
 });
